b20d01ea2413cd21f70dfcfedc3d99af65931ce4
Sheickh <micigan806@gmail.com>
2026-05-28 16:52:03 +0300

    дополнил репозиторий, добавил GetAll

--- internal/repository/medicine_repository.go
+++ internal/repository/medicine_repository.go
@@ -1,16 +1,18 @@
 package repository
 
 import (
+	"errors"
 	"pharmacy-project/internal/models"
 
 	"gorm.io/gorm"
 )
 
 type MedicineRepository interface {
-	GetByID(id uint) error
+	GetAll() ([]models.Medicine, error)
+	GetByID(id uint) (*models.Medicine, error)
 	DeleteByID(id uint) error
 	CreateMed(req *models.MedCreateRequest) error
-	Patch(id uint, req models.MedUpdateRequest) error
+	MedUpdate(id uint, req models.MedUpdateRequest) error
 }
 
 type gormMedicineRepository struct {
@@ -21,8 +23,23 @@ func NewMedicineRepository(db *gorm.DB) MedicineRepository {
 	return &gormMedicineRepository{db: db}
 }
 
-func (r *gormMedicineRepository) GetByID(id uint) error {
-	return r.db.First(&models.Medicine{}, id).Error
+func (r *gormMedicineRepository) GetAll() ([]models.Medicine, error) {
+	var medicine []models.Medicine
+	if err := r.db.Find(&medicine).Error; err != nil {
+		return nil, err
+	}
+	return medicine, nil
+}
+
+func (r *gormMedicineRepository) GetByID(id uint) (*models.Medicine, error) {
+	var med models.Medicine
+	if err := r.db.First(&med, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, gorm.ErrRecordNotFound
+		}
+		return nil, err
+	}
+	return &med, nil
 }
 
 func (r *gormMedicineRepository) DeleteByID(id uint) error {
@@ -30,9 +47,9 @@ func (r *gormMedicineRepository) DeleteByID(id uint) error {
 }
 
 func (r *gormMedicineRepository) CreateMed(req *models.MedCreateRequest) error {
-	return r.db.Create(req).Error
+	return r.db.Create(&req).Error
 }
 
-func (r *gormMedicineRepository) Patch(id uint, req models.MedUpdateRequest) error {
-	return r.db.First(&models.Medicine{}, id).Updates(req).Error
+func (r *gormMedicineRepository) MedUpdate(id uint, req models.MedUpdateRequest) error {
+	return r.db.First(&models.Medicine{}, id).Updates(&req).Error
 }
