6db29aaf6439d14b2cfc10c2108102dae3c63d07
Rasul <overlord.ove@mail.ru>
2026-06-03 11:13:42 +0300

    review handler и заглушки временно

--- cmd/pharmacy/main.go
+++ cmd/pharmacy/main.go
@@ -11,22 +11,31 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type noopPurchaseValidator struct{}
+
+func (noopPurchaseValidator) UserPurchasedMedicine(userID, medicineID uint) (bool, error) {
+	return true, nil
+}
+
 func main() {
 	db := config.SetUpDatabaseConnection()
 
-	if err := db.AutoMigrate(&models.Cart{}, &models.User{}, &models.Medicine{}); err != nil {
+	if err := db.AutoMigrate(&models.Cart{}, &models.User{}, &models.Medicine{}, &models.Review{}); err != nil {
 		log.Fatalf("не удалось выполнить миграции: %v", err)
 	}
 
 	cartRepo := repository.NewCartRepository(db)
 	medRepo := repository.NewMedicineRepository(db)
 	userRepo := repository.NewUserRepository(db)
+	reviewRepo := repository.NewReviewRepository(db)
 
 	cartService := services.NewCartService(cartRepo, medRepo, userRepo)
-  medService := services.NewMedicineService(medRepo)
-  
+	medService := services.NewMedicineService(medRepo)
+	reviewService := services.NewReviewService(reviewRepo, userRepo, medRepo, noopPurchaseValidator{}, nil)
+	userService := services.NewUserService(userRepo)
+
 	router := gin.Default()
-	transport.RegisterRoutes(router, cartService, medService)
+	transport.RegisterRoutes(router, medService, cartService, userService, reviewService)
 
 	if err := router.Run(); err != nil {
 		log.Fatalf("не удалось запустить HTTP-сервер: %v", err)
