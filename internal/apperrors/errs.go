db44457a476fc81277dfed83f779b4bff7932788
Rasul <overlord.ove@mail.ru>
2026-06-03 11:00:32 +0300

    add review service

--- internal/apperrors/errs.go
+++ internal/apperrors/errs.go
@@ -3,9 +3,9 @@ package apperrors
 import "errors"
 
 var (
-	ErrUserNotFound      = errors.New("пользователь не найден")
-	ErrUserAlreadyExists = errors.New("пользователь с таким email или телефоном уже существует")
-	ErrInvalidUserInput  = errors.New("некорректные данные пользователя")
+	ErrUserNotFound       = errors.New("пользователь не найден")
+	ErrUserAlreadyExists  = errors.New("пользователь с таким email или телефоном уже существует")
+	ErrInvalidUserInput   = errors.New("некорректные данные пользователя")
 	ErrMedicineNotFound   = errors.New("лекарство с таким ID не найдено")
 	ErrItemNotFound       = errors.New("лекарства с таким номером нет в корзине")
 	ErrMedicineOutOfStock = errors.New("лекарства нет в наличии")
@@ -14,4 +14,7 @@ var (
 	ErrUserIDMismatch     = errors.New("user_id в URL и в теле запроса не совпадают")
 	ErrCartEmpty          = errors.New("вы еще ничего не добавили в корзину")
 	ErrItemAlreadyInCart  = errors.New("товар уже в корзине, измените количество")
+	ErrReviewNotAllowed   = errors.New("оставить отзыв можно только на купленное лекарство")
+	ErrInvalidReviewInput = errors.New("некорректные данные отзыва")
+	ErrReviewNotFound     = errors.New("отзыв не найден")
 )
