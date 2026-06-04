d6677bcb36f70692f98c7247927f4dc0edf0a6c7
Sheickh <micigan806@gmail.com>
2026-06-04 15:59:18 +0300

    Medicine implementation

--- internal/models/promocode.go
+++ internal/models/promocode.go
@@ -9,27 +9,46 @@ import (
 type Discount string
 
 const (
-	typePercent Discount = "percent"
-	typeFixed   Discount = "fixed"
+	TypePercent Discount = "percent"
+	TypeFixed   Discount = "fixed"
 )
 
 type Promocode struct {
 	gorm.Model
-	Code          string    `gorm:"unique" json:"code" binding:"required"`
-	Description   string    `json:"description" binding:"required"`
-	DiscountType  Discount  `json:"discountType" binding:"required"`
-	DiscountValue int       `json:"discountValue" binding:"required,gt=0"`
-	ValidFrom     time.Time `json:"validFrom" binding:"required"`
-	ValidTo       time.Time `json:"validTo" binding:"required"`
-	IsActive      bool      `json:"isActive"`
+	Code           string    `gorm:"unique" json:"code" binding:"required"`
+	Description    string    `json:"description" binding:"required"`
+	DiscountType   Discount  `json:"discountType" binding:"required"`
+	DiscountValue  float64   `json:"discountValue" binding:"required,gt=0"`
+	MaxUses        int       `json:"maxUse" binding:"required,gt=0"`
+	UsedCount      int       `json:"usedCount"`
+	MaxUsesPerUser int       `json:"maxUsesPerUser"`
+	ValidFrom      time.Time `json:"validFrom" binding:"required"`
+	ValidTo        time.Time `json:"validTo" binding:"required"`
+	IsActive       bool      `json:"isActive"`
 }
 
-type PromocodeUpsert struct {
-	Code          string    `json:"code" binding:"required"`
-	Description   string    `json:"description" binding:"required"`
-	DiscountType  Discount  `json:"discountType" binding:"required"`
-	DiscountValue int       `json:"discountValue" binding:"required,gt=0"`
-	ValidFrom     time.Time `json:"validFrom" binding:"required"`
-	ValidTo       time.Time `json:"validTo" binding:"required"`
-	IsActive      bool      `json:"isActive"`
+type PromocodeCreate struct {
+	Code           string    `json:"code" binding:"required"`
+	Description    string    `json:"description" binding:"required"`
+	DiscountType   Discount  `json:"discountType" binding:"required"`
+	DiscountValue  float64   `json:"discountValue" binding:"required,gt=0"`
+	MaxUses        int       `json:"maxUse" binding:"required,gt=0"`
+	UsedCount      int       `json:"usedCount"`
+	MaxUsesPerUser int       `json:"maxUsesPerUser"`
+	ValidFrom      time.Time `json:"validFrom" binding:"required"`
+	ValidTo        time.Time `json:"validTo" binding:"required"`
+	IsActive       bool      `json:"isActive"`
+}
+
+type PromocodeUpdate struct {
+	Code           *string    `json:"code" binding:"required"`
+	Description    *string    `json:"description" binding:"required"`
+	DiscountType   *Discount  `json:"discountType" binding:"required"`
+	DiscountValue  *float64   `json:"discountValue" binding:"required,gt=0"`
+	UsedCount      *int       `json:"usedCount"`
+	MaxUsesPerUser *int       `json:"maxUsesPerUser"`
+	MaxUses        *int       `json:"maxUse" binding:"required,gt=0"`
+	ValidFrom      *time.Time `json:"validFrom" binding:"required"`
+	ValidTo        *time.Time `json:"validTo" binding:"required"`
+	IsActive       *bool      `json:"isActive"`
 }
